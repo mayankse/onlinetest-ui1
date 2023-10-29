2d6535cedd52141f0a8581b0874fb37f5348bc9c
mayankse <mayank.setiya@gmail.com>
2023-10-29 11:08:23 +0530

    url change1

--- src/app/common/services/ServerCAll.js
+++ src/app/common/services/ServerCAll.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL="http://localhost:2020/"
+//const BASE_URL="http://localhost:2020/"
+const BASE_URL="https://server1-zeta.vercel.app/"
 class ServerCall
 {
     static fnSendGetReq(url)
